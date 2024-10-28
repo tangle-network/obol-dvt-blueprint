8eb2cffed4a48ad59f2a09366ee2259de7758a84
Serial <69764315+Serial-ATA@users.noreply.github.com>
2024-10-28 10:44:13 -0400

    chore: remove Docker entrypoint script

--- docker/entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-exec /usr/local/bin/obol-cluster-blueprint run \
-      --blueprint-id="$BLUEPRINT_ID" \
-      --service-id="$SERVICE_ID" \
-      --bind-addr="$BIND_ADDR" \
-      --bind-port="$BIND_PORT" "$@"
